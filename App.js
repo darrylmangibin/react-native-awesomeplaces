eb7dc8aea9e23c85099fdea6c43cdc55691b9ade
Darryl <darrylmangibin@gmail.com>
2019-04-17 15:40:05 +0800

    Positioning elements with flexbox

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet, TextInput } from 'react-native';
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
 
 class App extends React.Component {
   state = {
@@ -14,12 +14,16 @@ class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <TextInput 
-          style={{width: 300}}
-          placeholder="An awesome place"
-          value={this.state.placeName}
-          onChangeText={this.placeNameChangedHandler}
-        />
+        <View style={styles.inputContainer}>
+          <TextInput 
+            placeholder="An awesome place"
+            value={this.state.placeName}
+            onChangeText={this.placeNameChangedHandler}
+          />
+          <Button 
+            title="Add"
+          />
+        </View>
       </View>
     )
   }
@@ -32,6 +36,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#fff',
     padding: 26
+  }, 
+  inputContainer: {
+    // flex: 1,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  placeInput: {
+    width: '70%'
+  },
+  placeButton: {
+    width: '30%'
   }
 });
 
